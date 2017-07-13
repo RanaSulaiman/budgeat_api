c27a97d26585d3d858b24e6f5b62a64d9ca73af0
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-07-12 18:45:21 -0700

    Added show method to the controller, created the route to render the information for a specific recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,9 +2,19 @@ class RecipesController < ApplicationController
   def index
     recipes = Recipe.all
       if recipes.length > 1
-        render json: recipes.as_json(except: [:created_at, :updated_at]), status: :ok 
+        render json: recipes.as_json(except: [:created_at, :updated_at]), status: :ok
       else
         render json: {no_recipes: "Recipes were not found"}, status: :not_found
       end
   end
+
+  def show
+    recipe = Recipe.find_by(recipe_id: params[:recipe_id])
+    if recipe
+      render json:recipe.as_json(except: [:created_at,:updated_at]),status: :ok
+    else
+      render json: {errors: {recipe_id: ["recipe '#{params[:recipe_id]}' not found"]} }, status: :not_found
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # get "/zomg", to: "recipes#zomg"
 
-  get "/recipes", to:"recipes#index", as: "recipes"
+    get "/recipes", to:"recipes#index", as: "recipes"
+    get "/recipes/:recipe_id", to:"recipes#show", as: "recipe"
+
+
 
 end
