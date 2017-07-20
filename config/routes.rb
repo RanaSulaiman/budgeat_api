8809ae28114f616826b223ba521eed07292bd189
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-07-20 11:01:20 -0700

    Added default rails route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+    root to: "recipes#index"
+
     get "/recipes", to:"recipes#index", as: "recipes"
     get "/recipes/:recipe_id", to:"recipes#show", as: "recipe"
 
